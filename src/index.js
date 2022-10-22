3a801485c97f60b4bb2f252045c393f3df9664da
Kangdy <canfrog@naver.com>
2022-10-22 20:36:40 +0900

    Install morgan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 4000;
@@ -12,8 +13,10 @@ const handleHome = (req, res) => {
     return res.send("앙기모찌버터~");
 }
 
+app.use(morgan("dev"));
 app.get("/", logger, handleHome);
 
-const handleListening = () => console.log(`✔️ Server listening on port http://localhost:${PORT} 🚀`);
+const handleListening = () => 
+    console.log(`✔️ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);  
